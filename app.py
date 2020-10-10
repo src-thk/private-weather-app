501c794dc1a6eda5e14de3724436ca3523997ece
src-thk <sarthak-tandon@outlook.com>
2020-10-10 13:35:03 +0530

    Added error handling and custom error pages

--- app.py
+++ app.py
@@ -77,10 +77,27 @@ def index():
 
     return render_template("index.html")
 
+
 @app.route('/about')
 def about():
     return render_template("about.html")
 
+#error handling and custom pages
+@app.errorhandler(404)
+def error404(error):
+    return render_template("error.html"), 404
+
+@app.errorhandler(403)
+def error403(error):
+    return render_template("error.html"), 403
+
+@app.errorhandler(410)
+def error410(error):
+    return render_template("error.html"), 410
+
+@app.errorhandler(500)
+def error500(error):
+    return render_template("error.html"), 500
 
 if __name__ == "__main__":
     app.run()
