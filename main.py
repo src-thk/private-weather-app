67eba09fb3cfc4785f374ab5d70e44171746a550
src-thk <sarthak-tandon@outlook.com>
2020-10-10 13:03:23 +0530

    Added background logic

--- main.py
+++ main.py
@@ -30,23 +30,50 @@ def index():
         url = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city_name},{country_name}&appid={get_api_key()}&units=metric")
         weather_result = url.json()
 
-        #stores data to variables from API
-        city = str(weather_result['name'])
-        temp = int(weather_result['main']['temp'])
-        temp_min = int(weather_result['main']['temp_min'])
-        temp_max = int(weather_result['main']['temp_max'])
-        feels_like = int(weather_result['main']['feels_like'])
-        discription = str(weather_result['weather'][0]['description'])
-        visibility = int(weather_result['visibility'])
-        humidity = int(weather_result['main']['humidity'])
-        wind_speed = int(weather_result['wind']['speed'])
-        #background change depends on this
-        background_id = int(weather_result['weather'][0]['id'])
+        #checks for invalid data
+        if int(weather_result['cod']) == 404:
+            return render_template("error.html")
+        else:
+            #stores data to variables from API
+            city = str(weather_result['name'])
+            temp = int(weather_result['main']['temp'])
+            temp_min = int(weather_result['main']['temp_min'])
+            temp_max = int(weather_result['main']['temp_max'])
+            feels_like = int(weather_result['main']['feels_like'])
+            discription = str(weather_result['weather'][0]['description'])
+            visibility = int(weather_result['visibility'])
+            humidity = int(weather_result['main']['humidity'])
+            wind_speed = int(weather_result['wind']['speed'])
+            #background change depends on this
+            background_id = int(weather_result['weather'][0]['id'])
 
-        #change background image url
+            #logic to change background acc to weather
+            if background_id >= 200 and background_id <= 232:
+                background_image = "thunderstorm.svg"
 
-        #render result page
-        return render_template("result.html", city=city, temp=temp, temp_min=temp_min, temp_max=temp_max, feels_like=feels_like, discription=discription, visibility=visibility, humidity=humidity, wind_speed=wind_speed)
+            elif background_id >= 300 and background_id <= 321:
+                background_image = "drizzle.svg"
+
+            elif background_id >= 500 and background_id <= 531:
+                background_image = "raining.svg"
+
+            elif background_id >= 600 and background_id <= 622:
+                background_image = "snow.svg"
+
+            elif background_id >= 701 and background_id <= 781:
+                background_image = "atmostphere.svg"
+
+            elif background_id == 800:
+                background_image = "clear.svg"
+
+            elif background_id >= 801 and background_id <= 804:
+                background_image = "cloudy.svg"
+
+            else:
+                background_image = "clear.svg"
+
+            #render result page
+            return render_template("result.html", city=city, temp=temp, temp_min=temp_min, temp_max=temp_max, feels_like=feels_like, discription=discription, visibility=visibility, humidity=humidity, wind_speed=wind_speed, background_image=background_image)
 
     return render_template("index.html")
 
@@ -56,4 +83,4 @@ def about():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='localhost', port=9874)
